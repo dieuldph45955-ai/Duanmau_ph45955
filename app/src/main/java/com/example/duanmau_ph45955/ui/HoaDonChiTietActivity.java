09af4995e840422573ed4321718f05d6f9c01314
Dieu <dieuldph45955@fpt.edu.vn>
2026-03-23 17:32:47 +0700

    giao diện hóa đơn chi tiết

---
+++ app/src/main/java/com/example/duanmau_ph45955/ui/HoaDonChiTietActivity.java
@@ -0,0 +1,22 @@
+package com.example.duanmau_ph45955.ui;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.duanmau_ph45955.R;
+
+public class HoaDonChiTietActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_hoa_don_chi_tiet);
+
+    }
+}
